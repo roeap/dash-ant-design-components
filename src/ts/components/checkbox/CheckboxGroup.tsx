350e076bb75eec7443b60e29501bf75a8b79cfc7
Robert Pack <robstar.pack@gmail.com>
2022-05-04 23:10:46 +0200

    add CheckboxGroup component

---
+++ src/ts/components/checkbox/CheckboxGroup.tsx
@@ -0,0 +1,52 @@
+import React, { ReactNode, useCallback } from "react";
+import { DashComponentProps } from "../../props";
+import { Checkbox } from "antd";
+
+const { Group } = Checkbox;
+
+type Option = {
+    label: string;
+    value: string;
+    disabled?: boolean;
+    style?: object;
+};
+
+type Props = {
+    /**
+     * Currently selected values
+     */
+    value: string[];
+    /**
+     * All options within the CheckboxGroup
+     */
+    options: string[] | number[] | Option[];
+    /**
+     * Disables all checkboxes within the group
+     */
+    disabled?: boolean;
+    /**
+     * Defines CSS styles which will override styles previously set.
+     */
+    style?: object;
+    /**
+     * Often used with CSS to style elements with common properties.
+     */
+    class_name?: string;
+} & DashComponentProps;
+
+const CheckboxGroup = (props: Props) => {
+    const { class_name, setProps, ...otherProps } = props;
+
+    const onChange = useCallback(
+        (checkedValues) => setProps({ value: checkedValues }),
+        [setProps]
+    );
+    return <Group className={class_name} onChange={onChange} {...otherProps} />;
+};
+
+CheckboxGroup.defaultProps = {
+    options: [],
+    value: [],
+};
+
+export default CheckboxGroup;
