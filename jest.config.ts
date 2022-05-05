0b9a17058c475bf4fc5505fc47b4206b1ea8d4f7
Robert Pack <robstar.pack@gmail.com>
2022-05-05 07:50:21 +0200

    add .venv to jest ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -26,9 +26,7 @@ export default {
     coverageDirectory: "coverage",
 
     // An array of regexp pattern strings used to skip coverage collection
-    // coveragePathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    coveragePathIgnorePatterns: ["/node_modules/", ".venv/"],
 
     // Indicates which provider should be used to instrument code for coverage
     // coverageProvider: "babel",
@@ -145,9 +143,7 @@ export default {
     // ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "/node_modules/"
-    // ],
+    testPathIgnorePatterns: ["/node_modules/", ".venv/"],
 
     // The regexp pattern or array of patterns that Jest uses to detect test files
     // testRegex: [],
